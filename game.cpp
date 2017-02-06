b028a729792c8c8fb61325633a7a5ed262928278
Szwarc Adam <adam.szwarc@nokia.com>
2017-02-06 20:32:20 +0100

    Lesson 5. Added spawning enemies and deleting them when reach end of screen

--- game.cpp
+++ game.cpp
@@ -1,6 +1,9 @@
 #include "game.h"
-#include <QGraphicsScene>
+#include "enemy.h"
 #include "myreact.h"
+
+#include <QGraphicsScene>
+#include <QTimer>
 
 Game::Game()
 {
@@ -9,17 +12,31 @@ Game::Game()
 
 void Game::setupElements()
 {
-    QGraphicsScene* scene = new QGraphicsScene();
-    view_ = std::make_shared<QGraphicsView>(scene);
+    scene_ = new QGraphicsScene();
+    view_ = std::make_shared<QGraphicsView>(scene_);
 
     view_->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view_->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     view_->show();
     view_->setFixedSize(width_, height_);
-    scene->setSceneRect(0, 0, width_, height_);
+    scene_->setSceneRect(0, 0, width_, height_);
 
     MyReact* rectItem = new MyReact(40, 50);
-    scene->addItem(rectItem);
+    scene_->addItem(rectItem);
     rectItem->setPos(view_->width() / 2, view_->height() - rectItem->rect().height());
+    spawnEnemies();
+}
+
+void Game::spawnEnemies()
+{
+    QTimer* timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(setupEnemies()));
+    timer->start(2000);
+}
+
+void Game::setupEnemies()
+{
+    Enemy* newEnemy = new Enemy();
+    scene_->addItem(newEnemy);
 }
--- game.h
+++ game.h
@@ -2,15 +2,22 @@
 #define GAME_H
 
 #include <QGraphicsView>
+#include <QObject>
 #include <memory>
 
-class Game
+class Game : public QObject
 {
+    Q_OBJECT
 public:
     Game();
     void setupElements();
+public slots:
+    void setupEnemies();
 private:
+    void spawnEnemies();
+
     std::shared_ptr<QGraphicsView> view_;
+    QGraphicsScene* scene_;
     int width_{800};
     int height_{600};
 };
