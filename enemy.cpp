e137f01ae89bc6b08ff89f24866e43d5eeded85a
ASzwarc <szwarc.adam@gmail.com>
2017-03-20 23:25:31 +0100

    Added enemy picture instead of box. Orientation of picture is wrong.

--- enemy.cpp
+++ enemy.cpp
@@ -6,19 +6,30 @@
 
 Enemy::Enemy(QObject *parent, Health* health):
     QObject(parent),
-    health_(health)
+    health_(health),
+    pixmap_(QPixmap(":/images/alien_spaceship.png"))
 {
     setPos(rand() % 750, 0);
-    setRect(0, 0, 30, 20);
+    setPixmap(pixmap_);
     QTimer* timer = new QTimer();
     connect(timer, SIGNAL(timeout()), this, SLOT(move()));
     timer->start(100);
 }
 
+int inline Enemy::getHeight()
+{
+    return pixmap_.rect().height();
+}
+
+int inline Enemy::getWidth()
+{
+    return pixmap_.rect().width();
+}
+
 void Enemy::move()
 {
     setPos(x(), y() + 5);
-    if (pos().y() + rect().height() > 600)
+    if (pos().y() + getHeight() > 600)
     {
         qDebug() << "Enemy deleted (out of screen)";
         health_->decrement();
--- enemy.h
+++ enemy.h
@@ -1,21 +1,23 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
-#include <QGraphicsRectItem>
+#include <QGraphicsPixmapItem>
 #include <QObject>
 
 #include "health.h"
 
-class Enemy : public QObject, public QGraphicsRectItem
+class Enemy : public QObject, public QGraphicsPixmapItem
 {
     Q_OBJECT
 public:
     explicit Enemy(QObject *parent = nullptr, Health* health = nullptr);
-
+    inline int getHeight();
+    inline int getWidth();
 public slots:
     void move();
 private:
     Health* health_;
+    QPixmap pixmap_;
 };
 
 #endif // ENEMY_H
